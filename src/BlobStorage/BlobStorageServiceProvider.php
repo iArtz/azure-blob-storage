cb4036cc1230ae49f021bcd16330df05b3501b81
iArtz <kanitphong@uservices-thailand.com>
2022-02-28 09:18:39 +0700

    Add service provider

---
+++ src/BlobStorage/BlobStorageServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BlobStorage;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlobStorageServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider
+     * 
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('blobStorage', Blob::class);
+    }
+}
